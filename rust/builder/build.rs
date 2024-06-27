f50b06ac3f102c5964c113761373316fce73cb6c
Jordan Santell <jordan@common.tools>
2024-06-27 12:40:45 -0700

    Initial fork of builder from usuba. Rename usuba-bundle to bundler. Initial tests and docs for builder.

---
+++ rust/builder/build.rs
@@ -0,0 +1,36 @@
+use std::process::Command;
+
+const TYPESCRIPT_SOURCE_DEPENDENCIES: &[&'static str] = &[
+    "package.json",
+    "package-lock.json",
+    "common/io/wit/io.wit",
+    "common/data/wit/data.wit",
+    "common/module/wit/module.wit",
+];
+
+fn main() {
+    if !Command::new("npm")
+        .arg("install")
+        .current_dir("../../typescript")
+        .status()
+        .unwrap()
+        .success()
+    {
+        panic!("Failed to run npm install");
+    }
+
+    if !Command::new("npm")
+        .arg("run")
+        .arg("build")
+        .current_dir("../../typescript")
+        .status()
+        .unwrap()
+        .success()
+    {
+        panic!("Failed to run npm build");
+    }
+
+    for fragment in TYPESCRIPT_SOURCE_DEPENDENCIES.iter() {
+        println!("cargo:rerun-if-changed=../../typescript/{fragment}");
+    }
+}
