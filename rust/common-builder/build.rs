e59ba315eb3d732721b9ddf40f5d4b2642e13d0d
Chris Joel <0xcda7a@gmail.com>
2024-06-29 07:58:22 -0700

    feat: Use shared `proto` directory

--- rust/common-builder/build.rs
+++ rust/common-builder/build.rs
@@ -2,11 +2,21 @@ use std::{env, path::PathBuf};
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let proto_path = manifest_dir
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("proto");
 
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("builder_descriptor.bin"))
-        .compile(&["proto/builder/builder.proto"], &["proto"])
+        .compile(&["builder/builder.proto"], &[proto_path.clone()])
         .unwrap();
 
-    println!("cargo:rerun-if-changed=./proto/builder/builder.proto");
+    println!(
+        "cargo:rerun-if-changed={}/builder/builder.proto",
+        proto_path.display()
+    );
 }
