edb91cbaf8fb1b7f701e60dd70834669662692dc
Chris Joel <0xcda7a@gmail.com>
2024-07-18 11:13:42 -0700

    chore: Perform Wasm artifact build AOT

--- rust/common-runtime/build.rs
+++ rust/common-runtime/build.rs
@@ -1,3 +1,4 @@
+use std::fs::copy;
 use std::{path::PathBuf, process::Command};
 
 fn main() {
@@ -6,20 +7,35 @@ fn main() {
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
 
     println!("cargo::rustc-env=COMMON_JAVASCRIPT_INTERPRETER_WASM_PATH={}/common_javascript_interpreter.wasm", out_dir.display());
-    println!(
-        "cargo::rerun-if-changed={}/rust/common-javascript-interpreter",
-        project_root_dir.display()
-    );
-    println!(
-        "cargo::rerun-if-changed={}/build/components",
-        project_root_dir.display()
-    );
 
-    if !Command::new("./scripts/generate-artifacts.sh")
-        .status()
-        .unwrap()
-        .success()
-    {
-        println!("Failed to generate required artifacts");
+    let cached_artifact_file = project_root_dir
+        .join(".wasm_cache")
+        .join("common_javascript_interpreter.wasm");
+
+    if cached_artifact_file.is_file() {
+        println!("cargo::rerun-if-changed={}", cached_artifact_file.display());
+
+        copy(
+            cached_artifact_file,
+            out_dir.join("common_javascript_interpreter.wasm"),
+        )
+        .unwrap();
+    } else {
+        println!(
+            "cargo::rerun-if-changed={}/rust/common-javascript-interpreter",
+            project_root_dir.display()
+        );
+        println!(
+            "cargo::rerun-if-changed={}/build/components",
+            project_root_dir.display()
+        );
+
+        if !Command::new("./scripts/generate-artifacts.sh")
+            .status()
+            .unwrap()
+            .success()
+        {
+            println!("Failed to generate required artifacts");
+        }
     }
 }
