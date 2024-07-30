5765f50e610d80ced5317cee181d1ab5a0e263ee
Jordan Santell <jsantell@users.noreply.github.com>
2024-07-30 10:51:37 -0700

    feat: Add interpreted modules to benchmark. (#81)

--- rust/common-runtime/benches/runtime_bench.rs
+++ rust/common-runtime/benches/runtime_bench.rs
@@ -39,10 +39,10 @@ impl BenchModule {
     }
 
     /// Module definition for [BASIC_MODULE_JS].
-    pub fn new_basic_js_module() -> Self {
+    pub fn new_basic_js_module(target: Target) -> Self {
         Self {
             module_source: ModuleSource {
-                target: Target::CommonModule.into(),
+                target,
                 source_code: [(
                     "module".to_owned(),
                     SourceCode {
@@ -64,32 +64,46 @@ fn run_benchmark(c: &mut Criterion) {
         .build()
         .unwrap();
 
-    let (runtime, instance_id, _builder_task) = async_runtime.block_on(async {
+    let (runtime, module_id, script_id, _) = async_runtime.block_on(async {
         let (builder_address, builder_task) = init_build_server().await.unwrap();
         let mut runtime = Runtime::new().unwrap();
-        let (module, initial_io) =
-            BenchModule::new_basic_js_module().into_components(Some(builder_address.clone()));
-        let instance_id = runtime.compile(module, initial_io).await.unwrap();
+        let module_id = {
+            let (module, initial_io) = BenchModule::new_basic_js_module(Target::CommonModule)
+                .into_components(Some(builder_address.clone()));
+            runtime.compile(module, initial_io).await.unwrap()
+        };
+        let script_id = {
+            let (module, initial_io) = BenchModule::new_basic_js_module(Target::CommonScript)
+                .into_components(Some(builder_address.clone()));
+            runtime.interpret(module, initial_io).await.unwrap()
+        };
 
-        (runtime, instance_id, builder_task)
+        (runtime, module_id, script_id, builder_task)
     });
 
     let bench_input = {
         let input = [("foo".into(), Value::String("updated foo".into()))].into();
-        let output_shape = runtime.output_shape(&instance_id).unwrap().to_owned();
+        let output_shape = runtime.output_shape(&module_id).unwrap().to_owned();
         RuntimeIo::new(input, output_shape)
     };
 
-    c.bench_with_input(
+    let mut group = c.benchmark_group("run_benchmark");
+
+    group.bench_with_input(
         BenchmarkId::new("Runtime::run_module", ""),
-        &bench_input,
+        &bench_input.clone(),
         |b, runtime_io| {
-            b.to_async(&async_runtime).iter(|| {
-                // As RuntimeIo is owned, containing dynamic BTreeMaps,
-                // we can't avoid either construction or cloning, responsible
-                // for ~70us
-                runtime.run(&instance_id, runtime_io.to_owned())
-            })
+            b.to_async(&async_runtime)
+                .iter(|| runtime.run(&module_id, runtime_io.to_owned()))
+        },
+    );
+
+    group.bench_with_input(
+        BenchmarkId::new("Runtime::run_script", ""),
+        &bench_input.clone(),
+        |b, runtime_io| {
+            b.to_async(&async_runtime)
+                .iter(|| runtime.run(&script_id, runtime_io.to_owned()))
         },
     );
 }
