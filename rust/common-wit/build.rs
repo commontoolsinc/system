b29fd3690db363a6677b81d402f8c99da768a0b4
Jordan Santell <jsantell@users.noreply.github.com>
2024-08-12 12:14:42 -0700

    chore: Remove wireit, build complexity from npm packages. Move wit definitions to root. (#94)

--- rust/common-wit/build.rs
+++ rust/common-wit/build.rs
@@ -1,36 +1,11 @@
-use std::process::Command;
-
-const TYPESCRIPT_SOURCE_DEPENDENCIES: &[&str] = &[
-    "package.json",
-    "package-lock.json",
+const WIT_DEPENDENCIES: &[&str] = &[
     "common/io/wit/io.wit",
     "common/data/wit/data.wit",
-    "common/module/wit/module.wit",
+    "common/function/wit/function.wit",
 ];
 
 fn main() {
-    if !Command::new("npm")
-        .arg("ci")
-        .current_dir("../../typescript")
-        .status()
-        .unwrap()
-        .success()
-    {
-        panic!("Failed to run npm install");
-    }
-
-    if !Command::new("npm")
-        .arg("run")
-        .arg("build")
-        .current_dir("../../typescript")
-        .status()
-        .unwrap()
-        .success()
-    {
-        panic!("Failed to run npm build");
-    }
-
-    for fragment in TYPESCRIPT_SOURCE_DEPENDENCIES.iter() {
-        println!("cargo:rerun-if-changed=../../typescript/{fragment}");
+    for fragment in WIT_DEPENDENCIES.iter() {
+        println!("cargo:rerun-if-changed=../../wit/{fragment}");
     }
 }
