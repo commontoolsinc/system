20489e8c59bd69dded5f0d3db9a7e7868b6399e6
Jordan Santell <jordan@common.tools>
2024-08-13 16:05:33 -0700

    feat: Add attribute helper to NewType derive macro to customize which traits and functions to implement.

---
+++ rust/common-macros/src/new_type.rs
@@ -0,0 +1,300 @@
+use darling::{FromDeriveInput, FromMeta};
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse_macro_input, Data, DeriveInput, Fields, Generics, Ident, ImplGenerics, Type,
+    TypeGenerics, WhereClause,
+};
+
+#[derive(PartialEq, Clone, Default, Debug, FromMeta)]
+#[darling(default)]
+struct Features {
+    #[darling(rename = "From")]
+    from: bool,
+    #[darling(rename = "Into")]
+    into: bool,
+    #[darling(rename = "Inner")]
+    inner: bool,
+    #[darling(rename = "Constructor")]
+    constructor: bool,
+    #[darling(rename = "Deref")]
+    deref: bool,
+}
+
+impl Features {
+    pub fn invert(mut self) -> Self {
+        self.from = !self.from;
+        self.into = !self.into;
+        self.inner = !self.inner;
+        self.constructor = !self.constructor;
+        self.deref = !self.deref;
+        self
+    }
+}
+
+#[derive(Default, PartialEq, Debug, FromDeriveInput)]
+#[darling(default, attributes(new_type), supports(struct_newtype))]
+struct NewTypeOpts {
+    skip: Option<Features>,
+    only: Option<Features>,
+}
+
+impl NewTypeOpts {
+    /// Returns the list of features to implement, normalized
+    /// to an `only` context.
+    ///
+    /// `only` takes precedence over `skip` if both defined.  
+    pub fn features(&self) -> Features {
+        match (self.skip.clone(), self.only.clone()) {
+            (Some(_), Some(only)) => only,
+            (None, Some(only)) => only,
+            (Some(skip), None) => skip.invert(),
+            (None, None) => Features::default().invert(),
+        }
+    }
+}
+
+struct ParsedInput {
+    name: Ident,
+    inner: Type,
+    generics: Generics,
+}
+
+impl ParsedInput {
+    fn expand(
+        &self,
+    ) -> (
+        &Ident,
+        &Type,
+        ImplGenerics,
+        TypeGenerics,
+        Option<&WhereClause>,
+    ) {
+        let (impl_generics, ty_generics, where_clause) = self.generics.split_for_impl();
+        (
+            &self.name,
+            &self.inner,
+            impl_generics,
+            ty_generics,
+            where_clause,
+        )
+    }
+}
+
+impl From<(Ident, Type, Generics)> for ParsedInput {
+    fn from(value: (Ident, Type, Generics)) -> Self {
+        ParsedInput {
+            name: value.0,
+            inner: value.1,
+            generics: value.2,
+        }
+    }
+}
+
+pub fn derive_new_type(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let opts = NewTypeOpts::from_derive_input(&input).unwrap();
+    let parsed = parse_new_type(input);
+
+    let mut impls: Vec<TokenStream> = vec![];
+    let features = opts.features();
+
+    if features.from {
+        impls.push(derive_from(&parsed));
+    }
+    if features.into {
+        impls.push(derive_into(&parsed));
+    }
+    if features.constructor {
+        impls.push(derive_constructor(&parsed));
+    }
+    if features.deref {
+        impls.push(derive_deref(&parsed));
+    }
+    if features.inner {
+        impls.push(derive_inner(&parsed));
+    }
+
+    impls.into_iter().collect()
+}
+
+/// Parses a [TokenStream], validates the new type,
+/// and returns a [ParsedInput].
+fn parse_new_type(input: DeriveInput) -> ParsedInput {
+    let name = input.ident;
+    let generics = input.generics;
+
+    let Data::Struct(data) = input.data else {
+        panic!("NewType only works for struct types.");
+    };
+
+    let Fields::Unnamed(fields) = data.fields else {
+        panic!("NewType requires new type structs.");
+    };
+
+    if fields.unnamed.len() != 1 {
+        panic!("Must be a new type with single inner type.");
+    }
+
+    let inner = fields.unnamed.first().unwrap().ty.clone();
+
+    (name, inner, generics).into()
+}
+
+fn derive_from(input: &ParsedInput) -> TokenStream {
+    let (name, inner, impl_generics, ty_generics, where_clause) = input.expand();
+    quote! {
+        impl #impl_generics ::core::convert::From<#inner> for #name #ty_generics #where_clause {
+            fn from(value: #inner) -> Self {
+                #name(value)
+            }
+        }
+    }
+    .into()
+}
+
+fn derive_into(input: &ParsedInput) -> TokenStream {
+    let (name, inner, impl_generics, ty_generics, where_clause) = input.expand();
+    quote! {
+        impl #impl_generics ::core::convert::From<#name #ty_generics> for #inner #where_clause {
+            fn from(value: #name #ty_generics) -> Self {
+                value.0
+            }
+        }
+    }
+    .into()
+}
+
+fn derive_inner(input: &ParsedInput) -> TokenStream {
+    let (name, inner, impl_generics, ty_generics, where_clause) = input.expand();
+    quote! {
+        impl #impl_generics #name #ty_generics #where_clause {
+            /// Returns the inner type.
+            pub fn inner(&self) -> &#inner {
+                &self.0
+            }
+
+            /// Returns the inner type.
+            pub fn inner_mut(&mut self) -> &mut #inner {
+                &mut self.0
+            }
+
+            /// Returns the inner type.
+            pub fn into_inner(self) -> #inner {
+                self.0
+            }
+        }
+    }
+    .into()
+}
+
+fn derive_deref(input: &ParsedInput) -> TokenStream {
+    let (name, inner, impl_generics, ty_generics, where_clause) = input.expand();
+    quote! {
+        impl #impl_generics ::core::ops::Deref for #name #ty_generics #where_clause {
+            type Target = #inner;
+
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl #impl_generics ::core::ops::DerefMut for #name #ty_generics #where_clause {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.0
+            }
+        }
+    }
+    .into()
+}
+
+fn derive_constructor(input: &ParsedInput) -> TokenStream {
+    let (name, inner, impl_generics, ty_generics, where_clause) = input.expand();
+    quote! {
+        impl #impl_generics #name #ty_generics #where_clause {
+            /// Creates a new [#name] from a [#inner].
+            pub fn new(inner: #inner) -> Self {
+                Self(inner)
+            }
+        }
+    }
+    .into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    type Result<T> = std::result::Result<T, String>;
+
+    fn parse(attrs: &str) -> Result<NewTypeOpts> {
+        let def = format!(
+            r#"
+        #[derive(NewType)]
+        {}
+        struct Foo(u32);"#,
+            attrs
+        );
+        FromDeriveInput::from_derive_input(&syn::parse_str(&def).map_err(|e| format!("{:#?}", e))?)
+            .map_err(|e| format!("Could not parse {}: {}", attrs, e))
+    }
+
+    #[test]
+    fn it_parses_attributes() -> Result<()> {
+        // Redundant "empty" attributes
+        for def in ["", "#[new_type]", "#[new_type()]"] {
+            assert_eq!(
+                parse(def)?,
+                NewTypeOpts {
+                    skip: None,
+                    only: None,
+                },
+            );
+        }
+
+        // Parses `only`
+        assert_eq!(
+            parse("#[new_type(only(From, Into))]")?,
+            NewTypeOpts {
+                skip: None,
+                only: Some(Features {
+                    from: true,
+                    into: true,
+                    ..Default::default()
+                }),
+            }
+        );
+
+        // Parses `skip`
+        assert_eq!(
+            parse("#[new_type(skip(Constructor))]")?,
+            NewTypeOpts {
+                skip: Some(Features {
+                    constructor: true,
+                    ..Default::default()
+                }),
+                only: None,
+            }
+        );
+
+        // Parseable, but derive macro will reject
+        // if both `only` and `skip` provided.
+        assert_eq!(
+            parse("#[new_type(only(From, Into), skip(Constructor))]")?,
+            NewTypeOpts {
+                skip: Some(Features {
+                    constructor: true,
+                    ..Default::default()
+                }),
+                only: Some(Features {
+                    from: true,
+                    into: true,
+                    ..Default::default()
+                }),
+            },
+            "Defaults to `only`."
+        );
+
+        Ok(())
+    }
+}
--- rust/common-macros/tests/new_type.rs
+++ rust/common-macros/tests/new_type.rs
@@ -1,35 +1,67 @@
 use common_macros::NewType;
 
-#[derive(NewType, PartialEq, Debug)]
-struct Foo(String);
-
 #[test]
-fn it_adds_methods_and_from_traits() {
-    let string: String = "foo".into();
-    let foo = Foo::from(string.clone());
+fn it_derives_from_inner() {
+    #[derive(NewType)]
+    struct Foo(pub String);
 
-    assert_eq!(foo, Foo::from(string.clone()));
-    assert_eq!(string, String::from(foo.clone()));
-    assert_eq!(foo.into_inner(), string);
+    let s = String::from("foo");
+    let foo = Foo::from(s.clone());
+    assert_eq!(foo.0, s);
 }
 
 #[test]
-fn it_adds_deref_traits() {
-    use std::ops::{Deref, DerefMut};
-    let string: String = "foo".into();
-    let foo = Foo::from(string.clone());
-    assert_eq!(&string, foo.deref());
+fn it_derives_into_inner() {
+    #[derive(NewType)]
+    struct Foo(pub String);
 
-    let mut foo = Foo::from(string.clone());
+    let s = String::from("foo");
+    let foo = Foo(s.clone());
+    let out: String = foo.into();
+    assert_eq!(out, s);
+}
+
+#[test]
+fn it_derives_deref() {
+    use std::ops::{Deref as DerefTrait, DerefMut as DerefMutTrait};
+    #[derive(Clone, NewType)]
+    struct Foo(pub String);
+
+    let s = String::from("foo");
+    let foo = Foo(s.clone());
+    assert_eq!(&s, foo.deref());
+
+    let mut foo = foo.clone();
     let mut_foo = foo.deref_mut();
     mut_foo.push_str("bar");
     assert_eq!(foo.deref(), &String::from("foobar"));
 }
 
 #[test]
-fn it_can_use_pub_types() {
-    #[derive(NewType)]
-    struct PubType(pub String);
+fn it_derives_constructor() {
+    #[derive(Clone, NewType)]
+    struct Foo(pub String);
+
+    let s = String::from("foo");
+    let foo = Foo::new(s.clone());
+    assert_eq!(foo.0, s);
+}
+
+#[test]
+fn it_derives_inner() {
+    #[derive(Clone, NewType)]
+    struct Foo(pub String);
+
+    let s = String::from("foo");
+    let mut foo = Foo(s.clone());
+    assert_eq!(foo.inner(), &s);
+
+    {
+        let inner = foo.inner_mut();
+        inner.push_str("bar");
+    }
+
+    assert_eq!(foo.into_inner(), String::from("foobar"));
 }
 
 #[test]
@@ -40,4 +72,58 @@ fn it_handles_generics() {
     let vec: Vec<u8> = vec![0];
     let buffer = Buffer::from(vec.clone());
     assert_eq!(vec, Vec::from(buffer));
+}
+
+#[test]
+fn it_only_includes_traits_helper() {
+    #[derive(NewType)]
+    #[new_type(only(From))]
+    struct Foo(pub String);
+
+    // This will fail if already defined
+    impl From<Foo> for String {
+        fn from(value: Foo) -> Self {
+            value.0
+        }
+    }
+
+    let s = String::from("foo");
+    let foo = Foo::from(s.clone());
+    assert_eq!(foo.0, s);
+}
+
+#[test]
+fn it_skips_includes_traits_helper() {
+    #[derive(NewType)]
+    #[new_type(skip(From))]
+    struct Foo(pub String);
+
+    // This will fail if already defined
+    impl From<String> for Foo {
+        fn from(value: String) -> Self {
+            Foo(value)
+        }
+    }
+
+    let s = String::from("foo");
+    let foo = Foo::from(s.clone());
+    assert_eq!(String::from(foo), s);
+}
+
+#[test]
+fn it_prioritizes_only_over_skip() {
+    #[derive(NewType)]
+    #[new_type(only(Constructor), skip(Constructor))]
+    struct Foo(pub String);
+
+    // This will fail if already defined
+    impl From<String> for Foo {
+        fn from(value: String) -> Self {
+            Foo(value)
+        }
+    }
+
+    let s = String::from("foo");
+    let foo = Foo::new(s.clone());
+    assert_eq!(foo.0, s);
 }
