6b12fa5184f7a3b414fe1aebc76e565e74797a96
Jordan Santell <jordan@common.tools>
2025-01-03 12:38:07 -0800

    chore: storage improvements. * Add range request utilities to Key and CtStorage * Change ranges to accept owned keys for easier stream functionality * CtStorage range tests * Leverage Key components as array references rather than slices

--- rust/ct-storage/src/lib.rs
+++ rust/ct-storage/src/lib.rs
@@ -2,13 +2,13 @@
 //! `ct-storage` is a passive database, currently a thin wrapper
 //! around `ranked-prolly-tree`.
 
+mod ct_storage;
 mod encoding;
 mod error;
 mod key;
-mod platform;
 mod storage;
 
+pub use ct_storage::*;
 pub use error::*;
 pub use key::*;
-pub use platform::PlatformStorage;
 pub use storage::*;
